2221426fbf7b28b2cc90a3c478c3015f36ebddbc
Cepoeympo <cepoeympo@gmail.com>
2016-05-30 18:11:23 +0300

    repear #ex height

--- js/768.js
+++ js/768.js
@@ -1,22 +1,7 @@
-/**
- * Created by Cepoeympo on 29.05.2016.
- */
-
 $(document).ready(function(){
-    var windowWidth = $(this).width();
+    var windowWidth = $(window).width();
 
     if(windowWidth<769){
         $('.certificate_link').addClass('disabled')
     }
 });
-
-
-/*
-
-$('.lightbox').addClass("lolol");
-
-data-lightbox="certificate"
-
-class='lightbox'
-
-    */
