1f52e8c02a214e81cb15a515b87f18969e56bbfb
Cepoeympo <cepoeympo@gmail.com>
2016-06-02 16:36:24 +0300

    ver. 0.3.0

--- games/js/guess.js
+++ games/js/guess.js
@@ -1,9 +1,9 @@
 window.onload = function () {
-    document.getElementById("num1").oninput = function() {
+    document.getElementById("num1").oninput = function () {
         document.getElementById("num2").value = document.getElementById("num1").value;
     };
 
-    document.getElementById("num2").oninput = function() {
+    document.getElementById("num2").oninput = function () {
         document.getElementById("num1").value = document.getElementById("num2").value;
     };
 
@@ -12,13 +12,13 @@ window.onload = function () {
     var i = 0;
 
     var num1 = document.getElementById("num1");
-    
-    document.getElementById('seven21').onclick = function(){
+
+    document.getElementById('seven21').onclick = function () {
 
         do {
-            if ( num1.value == rndm ){
+            if (num1.value == rndm) {
                 i++;
-                document.getElementById("result").innerHTML = 'НЛО не верит своим глазам... Ты сделал это c '+i+' раза! Ура-Ура-Ура!';
+                document.getElementById("result").innerHTML = 'НЛО не верит своим глазам... Ты сделал это c ' + i + ' раза! Ура-Ура-Ура!';
                 //document.getElementById("result").style.color = "#0973D9";
                 num1.style.boxShadow = 'inset 0px 0px 0px red';
                 num1.style.background = '#6CC000';
@@ -27,7 +27,7 @@ window.onload = function () {
                 rndm = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
                 i = 0;
                 break;
-            } else if ( (num1.value > rndm && num1.value <= 100) ){
+            } else if ((num1.value > rndm && num1.value <= 100)) {
                 document.getElementById("result").innerHTML = 'Ну это уже перебор.. Попробуй ещё раз!';
                 num1.style.boxShadow = 'inset 0px 0px 15px red';
                 num1.style.border = "2px solid #F00";
@@ -35,7 +35,7 @@ window.onload = function () {
                 num1.style.color = "#000";
                 i++;
                 break;
-            } else if ( (num1.value < rndm && num1.value >= 1) ){
+            } else if ((num1.value < rndm && num1.value >= 1)) {
                 document.getElementById("result").innerHTML = 'Маловато будет.. Попробуй ещё раз!';
                 num1.style.boxShadow = 'inset 0px 0px 15px #18B1F7';
                 num1.style.border = "2px solid #18B1F7";
@@ -47,7 +47,7 @@ window.onload = function () {
         } while (true);
     };
 
-    document.getElementById('lose').onclick = function(){
+    document.getElementById('lose').onclick = function () {
         document.getElementById("result").innerHTML = 'Эх, а счастье было так близко...';
         num1.style.boxShadow = 'inset 0px 0px 0px red';
         num1.style.border = "2px solid #000";
@@ -58,7 +58,7 @@ window.onload = function () {
         i = 0;
     };
 
-    document.getElementById('refresh').onclick = function(){
+    document.getElementById('refresh').onclick = function () {
         i = 0;
         document.getElementById("result").innerHTML = 'Ну попытайся ещё разок..';
         rndm = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
@@ -68,39 +68,39 @@ window.onload = function () {
         num1.style.border = "2px solid #000";
         num1.style.background = "#fff";
         num1.style.color = "#000";
-     };
+    };
 
 
 };
 
 /*
 
-var rndm = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-		
-function seven21(){
+ var rndm = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 
-	var i = 0;
+ function seven21(){
 
-	while (i != 666) {
-	
-		var userAnswer = prompt('Прелетело НЛО и загадало число от 1 до 100.. Угадаешь?','');
-		
-		if( (userAnswer == null) || (userAnswer.toLowerCase() == 'стоп') ){
-			alert('Ну и ну... Главное - не победа? Итого: '+ i +' участий =)');
-			break;
-		} else if ( userAnswer == rndm ){
-			alert('НЛО не верит своим глазам... Ты сделал это! С '+i+' раза!');
-			break;
-		} else if ( (userAnswer > rndm && userAnswer <= 100) ){
-			alert('Ну это уже перебор.. Попробуй ещё раз!');
-		} else if ( (userAnswer < rndm && userAnswer >= 1) ){
-			alert('Маловато будет.. Попробуй ещё раз!');
-		} else if( (userAnswer < 1) && (userAnswer != null) ){
-			alert('Читай лучше... НЛО загадало число больше 1!');
-		} else {
-			alert('Читай лучше... НЛО загадало число меньше 100!');
-		}
-		i++;
-	};
-}
-*/
+ var i = 0;
+
+ while (i != 666) {
+
+ var userAnswer = prompt('Прелетело НЛО и загадало число от 1 до 100.. Угадаешь?','');
+
+ if( (userAnswer == null) || (userAnswer.toLowerCase() == 'стоп') ){
+ alert('Ну и ну... Главное - не победа? Итого: '+ i +' участий =)');
+ break;
+ } else if ( userAnswer == rndm ){
+ alert('НЛО не верит своим глазам... Ты сделал это! С '+i+' раза!');
+ break;
+ } else if ( (userAnswer > rndm && userAnswer <= 100) ){
+ alert('Ну это уже перебор.. Попробуй ещё раз!');
+ } else if ( (userAnswer < rndm && userAnswer >= 1) ){
+ alert('Маловато будет.. Попробуй ещё раз!');
+ } else if( (userAnswer < 1) && (userAnswer != null) ){
+ alert('Читай лучше... НЛО загадало число больше 1!');
+ } else {
+ alert('Читай лучше... НЛО загадало число меньше 100!');
+ }
+ i++;
+ };
+ }
+ */
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 "use strict";
 
 var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
+
 var connect = require('gulp-connect');
 var livereload = require('livereload');
 var sass = require('gulp-sass');
 var uncss = require('gulp-uncss');
 var autoprefixer = require('gulp-autoprefixer');
-//var rename      = require("gulp-rename");
+var notify = require('gulp-notify');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var rename = require("gulp-rename");
+
 //var concatCss    = require('gulp-concat-css');
 
 // connect
@@ -25,36 +31,60 @@ gulp.task('html', function () {
 
 // js
 gulp.task('js', function () {
-    gulp.src('js/*.js')
+    gulp.src('source/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js'))
         .pipe(connect.reload());
 });
 
 // css
 gulp.task('css', function () {
-    gulp.src('first.scss')
+    gulp.src('source/first.scss')
         .pipe(sass())
         .pipe(autoprefixer({browsers: ['last 15 versions']}))
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest(''))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('css'))
         .pipe(connect.reload());
 });
 
 // uncss
 gulp.task('uncss', function () {
-    return gulp.src('bootstrap/css/*.css')
+    gulp.src('bootstrap/css/*.css')
         .pipe(uncss({
             html: ['*.html', 'games/*.html']
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('bootstrap/css/min/'))
+        //.pipe(notify({message: 'Gulp complete ^_^'}))
         .pipe(connect.reload());
 });
 
+/*
+// images
+gulp.task('images', function() {
+return gulp.src('source/*.jpg, source/*.png')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('./images/'))
+});
+*/
+
+/*
+// notify
+gulp.task('notify', function () {
+    gulp.src("")
+    .pipe(notify({message: 'Gulp complete ^_^'}));
+});
+*/
+
 // watch
 gulp.task('watch', function () {
-    gulp.watch('*.scss', ['css', 'uncss']);
+    gulp.watch('source/*.scss', ['css', 'uncss']);
     gulp.watch('*.html', ['html']);
-    gulp.watch('js/*.js', ['js']);
+    gulp.watch('source/*.js', ['js']);
 });
 
 // default
