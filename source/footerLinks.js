623bd7c6c2bb084c33f9969d7abcc39f6a8a1558
Cepoeympo <cepoeympo@gmail.com>
2016-06-07 22:06:26 +0300

    10.4.0 Modal message adding *PHP need to be fix*

--- source/footerLinks.js
+++
@@ -1,9 +0,0 @@
-$('#gameStart').click(function () {
-	$('#content').css('display', 'none');
-	$('#game_box').css('display', 'block');
-});
-
-$('#gameStop').click(function () {
-	$('#content').css('display', 'block');
-	$('#game_box').css('display', 'none');
-});
