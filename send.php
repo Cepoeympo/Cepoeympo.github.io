c45425fb4b271c24d7e3444f9f9acd8939f2f151
Cepoeympo <cepoeympo@gmail.com>
2016-06-08 08:32:27 +0300

    Message modal - yes

--- send.php
+++ send.php
@@ -1,4 +1,5 @@
 <?php
+/*
 if (isset($_POST['inputName'])) {
 	$inputName = $_POST['inputName'];
 }
@@ -18,48 +19,34 @@ $phoneModal = htmlspecialchars($phoneModal);
 $messageBody = stripslashes($messageBody);
 $messageBody = htmlspecialchars($messageBody);
 
-$address = "dakine13@ya.ru";
+$address = "79269902226@ya.ru";
 
-$message = "" . $messageBody . "\nС уважением, " . $inputName . "\nМой контактный e-mail: " . $phoneModal . "";
+$message = "" . $messageBody . "\nС уважением, " . $inputName . "\nМой контактный телефон: " . $phoneModal . "";
 
 $verify = mail($address, $message, "Content-type:text/plain; Charset=UTF-8");
 
 
 if ($verify == 'true') {
-	//echo "Письмо отправлено. Ответа не ждите :)";
-	header("Location: тут путь, куда перенаправлять");
-}
-
-/*
-if ($verify == 'true') {
-	echo "
-<link rel='stylesheet' type='text/css' href='style.css'/>
-<table width='600' align='center'>
- <tr>
- <td class='warning_table' width='220' align='center' valign='middle'>
-
- <!-- <img align='center' src='img/warning.png'> -->
- <div align='center' class='warning_font_big'>Поздравляем!</div>
- <div align='center' class='warning_font'>Ваше письмо доставлено администратору. Через некоторое время Вы получите ответ!</div>
- <p align='center'><a href='index.html' class='all_links'>Вернуться назад</a></div></p>
-
- </td>
- </tr>
- </table>";
-} else {
-	echo "
-<link rel='stylesheet' type='text/css' href='style.css'/>
-<table width='600' align='center'>
- <tr>
- <td class='warning_table' width='220' align='center' valign='middle'>
-
- <!-- <img align='center' src='img/warning.png'> -->
- <div align='center' class='warning_font_big'>ОШИБКА!!!</div>
- <div align='center' class='warning_font'>Ваше письмо не доставлено. Повторите отправку немного позже!</div>
- <p align='center'><a href='index.html' class='all_links'>Вернуться назад</a></div></p>
-
- </td>
- </tr>
- </table>";
+	header("refresh:3;url=http://cepoeympo.github.io/");
+	echo "<p>Сообщение отправлено!</p><p>Через 3 секунд Вы вернётесь назад</p><p> или нажмите на <a href='http://cepoeympo.github.io/'> эту ссылку</a></p><p>С уважением,</p><p>Андрей Максимович</p>";
 }
 */
+///////////////
+
+
+$phoneModal = $_POST['phoneModal'];
+$inputName = $_POST['inputName'];
+$messageBody = $_POST['messageBody'];
+
+$to = "79269902226@ya.ru";
+$subject = "Очень важное письмо";
+$text = "$messageBody\n\nС уважением,\n$inputName\n$phoneModal\n";
+
+$header .= "Content-type: text/html; charset=utf-8\r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$verify = mail($to, $subject, $text, $headers);
+
+if ($verify == 'true') {
+	header("refresh:0;url=http://cepoeympo.github.io/");
+	/*echo "<p>Сообщение отправлено!</p><p>Через 3 секунд Вы вернётесь назад</p><p> или нажмите на <a href='http://cepoeympo.github.io/'> эту ссылку</a></p><p>С уважением,</p><p>Андрей Максимович</p>";*/
+}
